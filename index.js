a4c5f5634679cd633a349ffa823a377c245a4206
Joseph Abreau <joseph.abreau@gmail.com>
2023-09-19 18:29:14 -0600

    complete lab

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+}
+add(1080, 2);
+
+function subtract(a, b) {
+    return a - b;
+}
+subtract(0, 311);
+
+function multiply(a, b) {
+    return a * b;
+}
+multiply(421614, 1);
+
+function divide(a, b) {
+    return a / b;
+}
+divide(1, 0.19);
+
+function increment(n) {
+    return n += 1;
+}
+increment(5);
+
+function decrement(n) {
+    return n -= 1;
+}
+increment(5);
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+makeInt(2.2);
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  parseFloat(2.33332);
